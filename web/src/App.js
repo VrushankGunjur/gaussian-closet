fe4d96120a69d5ede2a1711e32de31e3d7ef7097
Vrushank Gunjur <vrushankg@hotmail.com>
2024-05-19 16:32:52 -0700

    IP adapter infra

--- web/src/App.js
+++ web/src/App.js
@@ -22,6 +22,7 @@ const App = () => {
             width: 800,
             //backgroundImage: backgroundURL,
             backgroundImage:'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*'
+
         })
     )
 
